905a2abbb44dbf23dc2c70de06744b89e5c9e3f3
Pavel1992js <140919286+Pavel1992js@users.noreply.github.com>
2023-09-08 01:14:20 +0300

    createRequest.js

--- js/createRequest.js
+++ js/createRequest.js
@@ -1,26 +1,68 @@
-const createRequest = (options = {}) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(options.method || "POST", options.url, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(options.params);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-           try {
-             const response = JSON.parse(xhr.responseText);
-              for (let key in response) {
-                if (response[key].err) {
-                  alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
-                  return;
-                          }
-                      }
-                     options.callback(response);
-                  } catch (err) {
-                      alert(`Ошибка ответа: ${err.message}`);
-                  }
-              } else {
-                  alert(`Ошибка запроса: ${xhr.status} ${xhr.statusText}`);
-              }
-          }
-      };
-  };
+function createRequest(requestBodyString, requestSourceString = "", callback, uploadInfoIsNeed = false) {
+    let XMLH = new XMLHttpRequest();
+    XMLH.open("POST", "https://jscp-diplom.netoserver.ru/");
+    XMLH.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    XMLH.send(requestBodyString);
+
+    if (uploadInfoIsNeed) {
+        XMLH.upload.onprogress = function (event) {
+            console.log(`Sending data... ${event.loaded} of ${event.total} bytes`);
+        };
+
+        XMLH.upload.onerror = function () {
+            console.log("data error");
+        };
+    }
+
+    XMLH.onload = function () {
+        if (XMLH.status != 200) {
+            alert("error: " + XMLH.status);
+            return;
+        }
+
+        console.log(`${requestSourceString} - status request: ${XMLH.status} (${XMLH.statusText})`);
+        callback(XMLH.response);
+
+    };
+
+    XMLH.onerror = function () {
+        alert("request error");
+    };
+
+};
+
+function setItem(key, value) {
+    try {
+        return window.sessionStorage.setItem(key, value);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function getItem(key) {
+    try {
+        return window.sessionStorage.getItem(key);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function setJSON(key, value) {
+    try {
+        const jSon = JSON.stringify(value);
+
+        setItem(key, jSon);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function getJSON(key) {
+    try {
+        const jSon = getItem(key);
+
+        return JSON.parse(jSon);
+    } catch (e) {
+        console.error(e);
+    }
+}
